41c2a8ac0846483b9852673346abecd11a6532b4
Kevin <kevinjaegle@outlook.de>
2026-01-09 18:17:18 +0100

    refactor: split code into cmd and internal packages

---
+++ internal/blockchain/blockchain.go
@@ -0,0 +1,15 @@
+package blockchain
+
+type Blockchain struct {
+	Blocks []*Block
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{Blocks: []*Block{NewGenesisBlock()}}
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.Blocks[len(bc.Blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.Blocks = append(bc.Blocks, newBlock)
+}
